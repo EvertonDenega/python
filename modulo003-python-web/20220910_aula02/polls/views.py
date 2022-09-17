efd2dc057db8f05e85d98cd7639a4fa542d06029
Everton Denega Souza <evertonds@gmail.com>
2022-09-17 17:03:19 -0300

    atualizacao página de votos

--- modulo003-python-web/20220910_aula02/polls/views.py
+++ modulo003-python-web/20220910_aula02/polls/views.py
@@ -1,8 +1,9 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404     # Função que vai renderizar o template
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -36,8 +37,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Você está olhando os resultados da pergunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Você está votando na pergunta {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "Você deve escolher uma opção válida!"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

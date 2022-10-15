2fa9c4ddd1d043b35ea6fd7a5834d73e8b13ef61
Everton Denega Souza <evertonds@gmail.com>
2022-10-15 17:35:34 -0300

    projeto biblioteca

---
+++ modulo003-python-web/20221015_aula06/catalogo/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.views import generic
+
+from .models import Livro, CopiaLivro, Autor
+
+# function-based view
+def index(request):
+
+    qtd_livros = Livro.objects.count()
+    qtd_copias = CopiaLivro.objects.count()
+
+    # filter -> WHERE
+    qtd_copias_disponiveis = CopiaLivro.objects.filter(
+        status__exact='d'
+    ).count()
+
+    qtd_autores = Autor.objects.count()
+
+    context = {
+        'qtd_livros': qtd_livros,
+        'qtd_copias': qtd_copias,
+        'qtd_copias_disponiveis': qtd_copias_disponiveis,
+        'qtd_autores': qtd_autores
+    }
+
+    return render(request, 'catalogo/index.html', context=context)
+
+
+# class-based view
+class LivroListView(generic.ListView):
+    model = Livro
+    context_object_name = "livro_list"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Livro.objects.order_by("-id").all()
+
+
+class LivroDetailView(generic.DetailView):
+    model = Livro
+
+
+class AutorListView(generic.ListView):
+    model = Autor
+    context_object_name = "autor_list"
+
+
+class AutorDetailView(generic.DetailView):
+    model = Autor
